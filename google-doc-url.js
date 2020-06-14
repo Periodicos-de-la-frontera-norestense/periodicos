ccf650c0dde261cad79550e5bbe32ec3e1d8131e
rlozano1994 <rlozano1994@gmail.com>
2020-06-13 20:52:43 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRMn-7kO7WhuCfQC2YkRDFstId28YQ0dxDznzxkcoDPZX9y6mp3gTHp0o2RzBZpPzfdQHCoXuP2p6Hs/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZJZjTkEmbSIUC1tR9pKR4lUtg4KpPHUKLGBqHqGRtDE/edit#gid=0';
