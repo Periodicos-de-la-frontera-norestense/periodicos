009ff0560adefa5c44d8fc065dd5f5bb08ecea6f
rlozano1994 <rlozano1994@gmail.com>
2020-06-13 20:28:42 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZJZjTkEmbSIUC1tR9pKR4lUtg4KpPHUKLGBqHqGRtDE/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZJZjTkEmbSIUC1tR9pKR4lUtg4KpPHUKLGBqHqGRtDE/edit#gid=164271551';
